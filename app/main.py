3331e3bf4df3af18bcc20ef1120ac84c274ac5ff
千秋紫莹 <103925928+QianQiuZy@users.noreply.github.com>
2026-07-15 01:22:06 +0800

    补充关闭/docs、/openapi.json、/redoc

--- app/main.py
+++ app/main.py
@@ -35,7 +35,7 @@ async def lifespan(app: FastAPI):
             except asyncio.CancelledError:
                 pass
 
-app = FastAPI(lifespan=lifespan)
+app = FastAPI(lifespan=lifespan,docs_url=None, redoc_url=None, openapi_url=None)
 
 app.add_middleware(
     CORSMiddleware,
